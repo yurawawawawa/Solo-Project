b9518a3600eec2e08f2b34bc30df8d8866d2484f
Arga Yura Danendra <argayura@gmail.com>
2026-03-09 13:26:40 +0700

    Visualisasi Rock Paper Scissor ke Browser

---
+++ JavaScript From Scratch/DOM/script.js
@@ -0,0 +1,13 @@
+// Ambil elemen
+const btnRock = document.querySelector("#btn-rock");
+const btnPaper = document.querySelector("#btn-paper");
+const btnScissor = document.querySelector("#btn-scissor");
+
+const playerChoice = document.querySelector("#player-choice");
+const computerChoice = document.querySelector("#computer-choice");
+const hasilRonde = document.querySelector("#hasil-ronde");
+const scoreAkhir = document.querySelector("#score-akhir");
+
+// Function
+
+// Function End
